30e701c7ea8a03fc5e4309379bc2b2210fc6ece2
racsoraul <racsoraul@gmail.com>
2014-05-11 19:06:38 -0600

    Muestra menú de roles
    Muestra menú de los roles, pero sin los intent para los demás activities

--- src/sv/ues/fia/serviciosocialfia/ServicioSocialActivity.java
+++ src/sv/ues/fia/serviciosocialfia/ServicioSocialActivity.java
@@ -1,16 +1,21 @@
 package sv.ues.fia.serviciosocialfia;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
-public class ServicioSocialActivity extends Activity {
+public class ServicioSocialActivity extends ListActivity {
+	
+	String[] roles = {"Estudiante", "Tutor","Director"};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_servicio_social);
+		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, roles));
 	}
 
 	@Override
@@ -19,5 +24,12 @@ public class ServicioSocialActivity extends Activity {
 		return true;
 		
 	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+	}
+	
+	
 
 }
