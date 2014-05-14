e965a860f9d905aedb7ba26fb0668ef4e4bb5c87
Surv1 <cosme.92@gmail.com>
2014-05-14 20:52:24 -0200

    Nuevo activity AprobarReprobarProyecto , AutorizarProyectos funciona bien sin problemas

--- gen/sv/ues/fia/serviciosocialfia/R.java
+++ gen/sv/ues/fia/serviciosocialfia/R.java
@@ -1086,6 +1086,8 @@ containing a value of this type.
         public static final int images=0x7f020059;
     }
     public static final class id {
+        public static final int ClblContenidoNombreProyecto=0x7f05003d;
+        public static final int CtextViewNombreProyecto=0x7f05003c;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1097,19 +1099,19 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050045;
+        public static final int action_settings=0x7f050046;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int container=0x7f05003e;
+        public static final int container=0x7f050040;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
-        public static final int elija_rol=0x7f050044;
+        public static final int elija_rol=0x7f050045;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
@@ -1118,11 +1120,11 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int imageButton1=0x7f050040;
-        public static final int imageButton2=0x7f050041;
-        public static final int lblTituloAutorizarP=0x7f05003c;
+        public static final int imageButton1=0x7f050042;
+        public static final int imageButton2=0x7f050043;
+        public static final int lblTituloAutorizarP=0x7f05003e;
         public static final int listMode=0x7f050001;
-        public static final int listProyectosPendientes=0x7f05003d;
+        public static final int listProyectosPendientes=0x7f05003f;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
@@ -1148,9 +1150,8 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f050042;
-        public static final int textView2=0x7f05003f;
-        public static final int textView3=0x7f050043;
+        public static final int textView2=0x7f050041;
+        public static final int textView3=0x7f050044;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1207,19 +1208,23 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_autorizar_proyectos=0x7f030018;
-        public static final int activity_bdcontrol=0x7f030019;
-        public static final int activity_gestion_precios=0x7f03001a;
-        public static final int activity_servicio_social=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int activity_aprobar_reprobar_proyecto=0x7f030018;
+        public static final int activity_autorizar_proyectos=0x7f030019;
+        public static final int activity_bdcontrol=0x7f03001a;
+        public static final int activity_gestion_precios=0x7f03001b;
+        public static final int activity_servicio_social=0x7f03001c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
     }
     public static final class menu {
-        public static final int autorizar_proyectos=0x7f0c0000;
-        public static final int bdcontrol=0x7f0c0001;
-        public static final int gestion_precios=0x7f0c0002;
-        public static final int servicio_social=0x7f0c0003;
+        public static final int aprobar_reprobar_proyecto=0x7f0c0000;
+        public static final int autorizar_proyectos=0x7f0c0001;
+        public static final int bdcontrol=0x7f0c0002;
+        public static final int gestion_precios=0x7f0c0003;
+        public static final int servicio_social=0x7f0c0004;
     }
     public static final class string {
+        public static final int CDescripcionProyecto=0x7f0a001c;
+        public static final int ClblNombreProyecto=0x7f0a001b;
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
         public static final int abc_action_bar_home_description=0x7f0a0001;
@@ -1270,6 +1275,7 @@ containing a value of this type.
         public static final int lblConsultarPrecios=0x7f0a0015;
         public static final int lblGestionPrecios=0x7f0a0013;
         public static final int lblTituloAutorizarProyectos=0x7f0a0019;
+        public static final int title_activity_aprobar_reprobar_proyecto=0x7f0a001a;
         public static final int title_activity_autorizar_proyectos=0x7f0a0018;
         public static final int title_activity_bdcontrol=0x7f0a0010;
         public static final int title_activity_gestion_precios=0x7f0a0012;
